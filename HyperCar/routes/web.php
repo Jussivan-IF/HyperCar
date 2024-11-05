e122880d0acbf764d1f0823d68454397d4ecceb9
AllanOliveira2022 <115299780+AllanOliveira2022@users.noreply.github.com>
2024-11-05 16:27:37 -0300

    Criando as rotas e models

--- HyperCar/routes/web.php
+++ HyperCar/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CarroController;
+use App\Http\Controllers\ReservaController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Rotas de autenticação
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Rotas protegidas
+Route::middleware(['auth'])->group(function () {
+    Route::resource('carros', CarroController::class);
+    Route::resource('reservas', ReservaController::class);
 });
